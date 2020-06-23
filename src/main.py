44257c50f11c431e508136f65c5c1c0c39d99fb5
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-06-23 14:09:09 -0300

    corrigido o pegar filtro de idade

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ caso = int(input("digite 1 para criar m caso novo e 2 para tentar um anterior: "
 if(caso==1):
     file_name = input("Digite o nome do arquivo excel(sem o .xlsx): ")
     [name_column, email_column, age_column,run_number] = csv_from_excel(file_name)
-    age_filter = int(input("Digite a idade minima do filtro (<): "))
+    age_filter = int(input("Digite o ano minimo de nascimento do filtro: "))
     set_data_storage(name_column, email_column, age_column,age_filter,run_number)
 else:
     #tenho que fazer isso ser automatico
