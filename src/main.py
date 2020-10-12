e7adb8754c111f85581219974a6d5355916e45b3
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-10-12 19:09:29 -0300

    removi o filtro de idade e adicionei a lista de emails bloqueados

--- src/main.py
+++ src/main.py
@@ -5,16 +5,20 @@ log_start_configuration()
 caso = int(input("digite 1 para criar um caso novo e 2 para tentar um anterior: "))
 if(caso==1):
     file_name = input("Digite o nome do arquivo excel(sem o .xlsx): ")
-    [name_column, email_column, age_column,run_number] = csv_from_excel(file_name)
-    age_filter = int(input("Digite o ano minimo de nascimento do filtro: "))
-    set_data_storage(name_column, email_column, age_column,age_filter,run_number)
+    [name_column, email_column,run_number] = csv_from_excel(file_name,'queue')
+    set_data_storage(name_column, email_column,run_number)
 else:
     #tenho que fazer isso ser automatico
     
     run_number = int(input("digite o numero da pasta que deseja utilizar: "))
-    [name_column, email_column, age_column,age_filter] = read_data_storage(run_number)
+    [name_column, email_column] = read_data_storage(run_number)
+
+is_blocking = int(input("digite 1 para usar uma lista de emails bloqueados e 2 para não usar: "))
+if(is_blocking==1):
+    file_name = input("Digite o nome do arquivo excel(sem o .xlsx): ")
+    [blocked_mails_col] = blocked_csv(file_name,run_number)
 log_line()
 log_start_reading_queue()
-[filtered,sent,failed] = read_queue_data(name_column, email_column, age_column,age_filter,run_number)
+[filtered,sent,failed] = read_queue_data(name_column, email_column,run_number)
 log_line()
 log_results(filtered,sent,failed)
--- src/utils.py
+++ src/utils.py
@@ -6,10 +6,10 @@ import os
 import datetime
 
 #armazena os dados de onde estão as colunas dos dados selecionados
-def set_data_storage(name_column, email_column, age_column,age_filter,i):
+def set_data_storage(name_column, email_column,i):
     data_file = open('data/{}/data.csv'.format(str(i)), 'w')
     wr = csv.writer(data_file, quoting=csv.QUOTE_ALL)
-    wr.writerow([name_column, email_column, age_column,age_filter])
+    wr.writerow([name_column, email_column])
     data_file.close()
 
 #le os dados selecionados previamente
@@ -25,7 +25,8 @@ def read_data_storage(i):
 
 #cria uma tabela para armazenar os envios que deram errados
 def create_error_csv(i):
-    error_file = open('data/{}/error.csv'.format(str(i)), 'w')
+    #os.mkdir("data/{}/error.csv".format(str(i)))
+    error_file = open("data/{}/".format(str(i))+"error.csv", "w")
     wr = csv.writer(error_file, quoting=csv.QUOTE_ALL)
     f = open("data/"+str(i)+'/queue.csv', 'r')
     reader = csv.reader(f)
@@ -39,12 +40,11 @@ def create_error_csv(i):
     error_file.close()
     name_column = input("digite o numero da coluna de nomes: ")
     email_column = input("digite o numero da coluna de emails: ")
-    age_column = input("digite o numero da coluna de idades: ")
     f.close()
-    return [int(name_column),int(email_column),int(age_column) ,int(i)]
+    return [int(name_column),int(email_column) ,int(i)]
 
 #le o xlsx e transforma numa fila em csv para poder trabalhar no python
-def csv_from_excel(arq_name):
+def csv_from_excel(arq_name,new_name):
     i = 0
     created = False
     while(not created):
@@ -53,14 +53,33 @@ def csv_from_excel(arq_name):
             created = True
         except:
             i = i +1
+    #os.mkdir("data/{}/queue.csv".format(str(i)))
     read_file = pd.read_excel ("{}.xlsx".format(arq_name))
-    read_file.to_csv ('data/{}/queue.csv'.format(str(i)), index = None, header=True)
+    read_file.to_csv ("data/{}/".format(str(i))+new_name+".csv", index = None, header=True)
     return create_error_csv(i)
+
+def blocked_csv(arq_name,i):
+    read_file = pd.read_excel ("{}.xlsx".format(arq_name))
+    read_file.to_csv ("data/{}/".format(str(i))+"block.csv", index = None, header=True)
+    f = open("data/"+str(i)+'/block.csv', 'r', encoding='utf-8')
+    reader = csv.reader(f)
+    index = 0
+    for row in reader:
+        for column in row:
+            print(str(index) + " - " + column)
+            index = index +1
+        break
+    email_column = input("digite o numero da coluna de emails: ")
+    f.close()
     
+    return [int(email_column)]
+
+
+
 #le a lista de itens que faltam ser enviados
 #envia o email e e remove a linha da lista
 #caso não consiga enviar o email, remove da lista e manda adicionar na tabela error
-def read_queue_data(name_column,email_column,age_column,age_filter,i):    
+def read_queue_data(name_column,email_column,i):    
     filtered =0
     sent =0
     failed =0
@@ -72,17 +91,18 @@ def read_queue_data(name_column,email_column,age_column,age_filter,i):
         data.append(r)
     f.close()
     update_data = data
-    for rw in range(len(data)-1,-1,-1):
+    for rw in range(len(data)-1,0,-1):
         
         row = data[rw]
         if(row != []):
             try:
                 name = str(row[name_column])
                 email = str(row[email_column])
-                age = row[age_column].split('-')[0]
+                #age = row[age_column].split('-')[0]
                 
-                int(age)
-                print("nome: " + name + " / email: "+ email+ " / nascimento: "+ age)
+                #int(age)
+                print("nome: " + name + " / email: "+ email) 
+                # / nascimento: "+ age)
 
                 
                     #Retorno:
@@ -90,7 +110,8 @@ def read_queue_data(name_column,email_column,age_column,age_filter,i):
                     #1 = passou no filtro e foi enviado
                     #2 = passou no filtro e não foi enviado
                 
-                response = check_valid_email(name,email, int(age), age_filter)
+                response = check_valid_email(name,email,i)
+                #response = mail(name,email)
                 if(response==0):
                     update_data = update_queue(i,update_data,row)
                 if(response == 1):
@@ -123,17 +144,40 @@ def update_queue(i,data,row):
     return data
 
 #faz o filtro e se passar manda para enviar o email
-def check_valid_email(name,email,age,age_filter):
+def check_valid_email(name,email,i):
+    print('verificando se o email é valido')
+    try:
+        fi = open("data/"+str(i)+'/block.csv', 'r',encoding='utf-8')
+        reader = csv.reader(fi)
+        data = []
+        for r in reader:
+            data.append(r)
+        fi.close()
+        achou = False
+        for rw in range(1,len(data),1):
+        
+            row = data[rw]
+            print(row[1])
+            if(str(row[1])==str(email)):
+                achou = True
 
-    #a idade vai vir por data, tem que trabalhar esse dado
-    if age>age_filter:
-        print("Muito novo para receber o email")
-        return 0
-    else:
+        if achou:
+            print("email bloqueado")
+            return 0
+        else:
+            print('ok')
+            return mail(name,email)
+    
+    except:
+        print('ok, sem lista de bloqueados')
         return mail(name,email)
+    
+
 
 #adiciona o item enviado na tabela de erro
 def add_failed_list(row,i):
+    print("adicionando na lista de erros")
+    print(row)
     error_file = open('data/{}/error.csv'.format(str(i)), 'r')
     data = []
     rd = csv.reader(error_file)
